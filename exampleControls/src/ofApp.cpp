93289f8f78630732086a6ba45339bbff6da5cdc3
random <random@frauzufall.de>
2015-07-14 11:29:17 +0200

    renamed examples to be used on OSX and Windows

---
+++ exampleControls/src/ofApp.cpp
@@ -0,0 +1,158 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    panel1.setup("extended gui");
+    panel1.setBorderColor(ofColor::black);
+
+    /*
+     * minimal button and toggle
+     */
+    panel1.add(new ofxMinimalToggle(toggle_param.set("show header", true), 0, 30));
+    toggle_param.addListener(this, &ofApp::toggleGroupHeader);
+    panel1.add(new ofxMinimalButton("button", 0, 30));
+
+    /*
+     * rotary slider
+     */
+    rotary.setup("rotary");
+    rotary.setBorderColor(ofColor::blanchedAlmond);
+    rotary.add(new ofxFloatRotarySlider(slider_param.set("slider", 0.5, 0, 1), 66,66));
+    rotary.getControl("slider")->setFillColor(ofColor::white);
+    rotary.getControl("slider")->setBackgroundColor(ofColor::blanchedAlmond - ofColor(130));
+
+    /*
+     * matrix with only one allowed active toggle
+     */
+    matrix_params.push_back(ofParameter<bool>("only",false));
+    matrix_params.push_back(ofParameter<bool>("one",false));
+    matrix_params.push_back(ofParameter<bool>("toggle",false));
+    matrix_params.push_back(ofParameter<bool>("can",false));
+    matrix_params.push_back(ofParameter<bool>("be",false));
+    matrix_params.push_back(ofParameter<bool>("active",false));
+    matrix.setup("matrix",3);
+    for(uint i = 0; i < matrix_params.size(); i++) {
+        matrix.add(new ofxMinimalToggle(matrix_params.at(i)));
+    }
+    matrix.setBorderColor(ofColor::aquamarine);
+    matrix.setElementHeight(26);
+    matrix.allowMultipleActiveToggles(false);
+
+    panel1.add(&rotary);
+    panel1.add(&matrix);
+
+    /*
+     * horizontal panel with spacer
+     */
+    panel2.setup("horizontal", "", 260, 10);
+    panel2.setAlignHorizontal();
+    panel2.setBorderColor(ofColor::black);
+    panel2.add(new ofxToggle(toggle1_param.set("toggle1", false), 100, 30));
+    panel2.add(new ofxMinimalToggle(toggle2_param.set("toggle2", false), 0, 30));
+    panel2.add(new ofxMinimalToggle(toggle3_param.set("toggle3", false), 0, 30));
+    panel2.add(new ofxGuiSpacer(30));
+    panel2.add(new ofxMinimalToggle(toggle4_param.set("toggle4", false), 0, 30));
+
+    /*
+     * panel with canvas
+     */
+    panel3.setup("more controls", "", 260, 90);
+    img.load("images/ente.jpg");
+    canvas.setup("some texture", &img.getTexture());
+    panel3.add(&canvas);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    if(toggle1_param.get()) {
+        ofSetColor(ofColor::royalBlue);
+    }
+    else {
+        if(toggle2_param.get()) {
+            ofSetColor(ofColor::azure);
+        }
+        else {
+            if(matrix_params.at(0).get()) {
+                ofSetColor(ofColor::burlyWood);
+            }
+            else {
+                ofSetColor(ofColor::fromHex(0x2da1e3));
+            }
+        }
+    }
+
+    ofDrawRectangle(ofGetWindowRect());
+
+    ofSetColor(255);
+
+    panel1.draw();
+    panel2.draw();
+    panel3.draw();
+
+}
+
+void ofApp::toggleGroupHeader(bool &val) {
+    rotary.showHeader(val);
+    matrix.showHeader(val);
+    panel1.showHeader(val);
+    panel2.showHeader(val);
+    panel3.showHeader(val);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    switch(key) {
+    case 'f': {
+        ofToggleFullscreen();
+        break;
+    }
+    default: break;
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(ofMouseEventArgs &args){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(ofMouseEventArgs &args){
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(ofMouseEventArgs &args){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(ofMouseEventArgs &args){
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
---
+++ exampleControls/src/ofApp.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGuiExtended.h"
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void exit();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(ofMouseEventArgs &args);
+    void mouseDragged(ofMouseEventArgs &args);
+    void mousePressed(ofMouseEventArgs &args);
+    void mouseReleased(ofMouseEventArgs &args);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+private:
+
+    ofxPanelExtended panel1, panel2, panel3;
+    ofxGuiGroupExtended rotary;
+    ofxGuiMatrix matrix;
+
+    ofImage img;
+    ofxCanvas canvas;
+
+    ofParameter<bool> toggle_param;
+    ofParameter<float> slider_param;
+    ofParameter<bool> toggle1_param, toggle2_param, toggle3_param, toggle4_param;
+    vector<ofParameter<bool>> matrix_params;
+    ofParameterGroup g;
+
+    void toggleGroupHeader(bool &val);
+
+};
+
